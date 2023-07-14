57c7f369aa1ae48e694c257a2b9371613ee22540
d0d04m3 <63002469+d0d04m3@users.noreply.github.com>
2023-07-14 23:22:17 +0100

    Update index.js

--- index.js
+++ index.js
@@ -16,17 +16,43 @@ exec("find / | grep chromium", (error, stdout, stderr) => {
     console.log(`stdout: ${stdout}`);
 });
 const asyncHandler = fn => (req, res, next) => Promise.resolve(fn(req, res, next)).catch(next);
-const startPuppeteerSession = async () => { const browser = await puppeteer.launch({
-    headless: false,
-    args: ['--no-sandbox', '--disable-setuid-sandbox'] 
-      
-    });
-  const page = await browser.newPage();
-  return {browser, page};
-};
 const sessions = {};
 
 
+
+ app.get("/demo", asyncHandler(async (req, res) => {
+/* start */
+     exports.handler = async (event, context, callback) => {
+  let result = null;
+  let browser = null;
+
+  try {
+    browser = await chromium.puppeteer.launch({
+      args: chromium.args,
+      defaultViewport: chromium.defaultViewport,
+      executablePath: await chromium.executablePath,
+      headless: chromium.headless,
+      ignoreHTTPSErrors: true,
+    });
+
+    let page = await browser.newPage();
+
+    await page.goto(event.url || 'https://google.com');
+
+    result = await page.title();
+  } catch (error) {
+    return callback(error);
+  } finally {
+    if (browser !== null) {
+      await browser.close();
+    }
+  }
+
+  return callback(null, result);
+};
+
+ /* end */  
+  }))
   app.use((req, res, next) => 
     req.query.token === undefined ? res.sendStatus(401) : next()
   )
@@ -58,12 +84,13 @@ const sessions = {};
    // console.log(sessions);
     res.send(await page.content()); 
   }))
-  .get("/kill", asyncHandler(async (req, res) => {
+  app.get("/kill", asyncHandler(async (req, res) => {
     const browser = await sessions[req.query.token].browser;
     await browser.close();
     delete sessions[req.query.token];
    // console.log(sessions);
     res.sendStatus(200);
   }))
+
   app.use((err, req, res, next) => res.sendStatus(500))
   app.listen(3000, () => console.log("listening on port 3000"));
