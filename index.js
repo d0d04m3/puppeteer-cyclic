01078f2334e196532b9cc7c9500e456fbefcdcd4
d0d04m3 <63002469+d0d04m3@users.noreply.github.com>
2023-07-14 23:06:11 +0100

    Update index.js

--- index.js
+++ index.js
@@ -3,6 +3,18 @@ const express = require("express");
 const path = require("path");
 const app = express(); // Initializing Express
 const puppeteer = require("puppeteer");
+const { exec } = require("child_process");
+exec("which chromium", (error, stdout, stderr) => {
+    if (error) {
+        console.log(`error: ${error.message}`);
+        return;
+    }
+    if (stderr) {
+        console.log(`stderr: ${stderr}`);
+        return;
+    }
+    console.log(`stdout: ${stdout}`);
+});
 const asyncHandler = fn => (req, res, next) => Promise.resolve(fn(req, res, next)).catch(next);
 const startPuppeteerSession = async () => { const browser = await puppeteer.launch({
     headless: false,
@@ -14,34 +26,6 @@ const startPuppeteerSession = async () => { const browser = await puppeteer.laun
 };
 const sessions = {};
 
-exports.handler = async (event, context, callback) => {
-  let result = null;
-  let browser = null;
-
-  try {
-    browser = await chromium.puppeteer.launch({
-      args: chromium.args,
-      defaultViewport: chromium.defaultViewport,
-      executablePath: await chromium.executablePath,
-      headless: chromium.headless,
-      ignoreHTTPSErrors: true,
-    });
-
-    let page = await browser.newPage();
-
-    await page.goto(event.url || 'https://google.com');
-
-    result = await page.title();
-  } catch (error) {
-    return callback(error);
-  } finally {
-    if (browser !== null) {
-      await browser.close();
-    }
-  }
-
-  return callback(null, result);
-};
 
   app.use((req, res, next) => 
     req.query.token === undefined ? res.sendStatus(401) : next()
